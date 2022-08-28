f2338565f40c9242c9a6f0084ee121e58bc5accb
Nanashi4 <86570343+Nanashi4@users.noreply.github.com>
2022-08-28 22:59:31 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+var threads = [
+{
+title: "Thread 1",
+author: "USER",
+date: Date.now(),
+content: "Thread content",
+comments: [
+{
+author: "USER2",
+date: Date.now(),
+content: "Unh"
+}
+]
+}
+]
